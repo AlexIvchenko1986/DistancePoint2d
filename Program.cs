1cf2fd6f900dba157c18c77effc09cd5987ff123
Sergei Kamianetskii <i@ksergey.ru>
2023-03-10 11:11:55 +0300

    Реализована работа с картой точек

--- Program.cs
+++ Program.cs
@@ -29,8 +29,59 @@ void SavePointListTo(string path, string data)
   File.WriteAllText(path, data);
 }
 
-int[,] point2dList = GetPoint2dList(10, 0, 20);
+int[,] LoadPointFromFile(string path)
+{
+  string[] lines = File.ReadAllLines(path);
+  int row = lines.Length;
+  int[,] table = new int[row, 2];
+  for (int i = 0; i < row; i++)
+  {
+    string[] point = lines[i].Split(';');
+    table[i, 0] = int.Parse(point[0]);
+    table[i, 1] = int.Parse(point[1]);
+  }
+  return table;
+}
 
-string data = PrinterList(point2dList);
+char[,] GetMap(int mapSize, int[,] points2d)
+{
+  char[,] map = new char[mapSize, mapSize];
+  int count = points2d.GetLength(0);
+  for (int i = 0; i < count; i++)
+  {
+    int x = points2d[i, 0];
+    int y = points2d[i, 1];
+    map[x, y] = 'x';
+  }
 
-SavePointListTo("points.csv", data);
+  return map;
+}
+
+string MapPrinter(char[,] map)
+{
+  int size = map.GetLength(0);
+  string result = String.Empty;
+
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      if (map[i, j] == 'x') result += $"x";
+      else result += $" ";
+    }
+    result += "\n";
+  }
+  return result;
+}
+
+int mapSize = 20;
+int pointCount = 10;
+//int[,] point2dList = GetPoint2dList(pointCount, 0, mapSize);
+int[,] point2dList = LoadPointFromFile("points.csv");
+// string data = PrinterList(point2dList);
+// SavePointListTo("points.csv", data);
+
+char[,] map = GetMap(mapSize, point2dList);
+string m = MapPrinter(map);
+System.Console.WriteLine(m);
+
